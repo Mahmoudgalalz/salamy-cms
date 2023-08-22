f73f4bdf23bbc6734883736996f5e45f9653fbeb
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-08-22 17:11:43 +0300

    allow cors and headers

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,11 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name:'strapi::cors',
+    config:{
+      origin:['*'],
+      headers:['*']
+    }
+  }
 ];
