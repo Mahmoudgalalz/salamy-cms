43e6550e3a49d7492bb33937abfd8fe2141944fe
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-12-27 14:48:25 +0200

    chore: updating dependancies

--- config/plugin.ts
+++ config/plugin.ts
@@ -1,4 +1,14 @@
 export default ({env}) => ({
+  seo: {
+    enabled: true,
+  },
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '7d',
+      },
+    },
+  },
     "fuzzy-search": {
         enabled: true,
         config: {
